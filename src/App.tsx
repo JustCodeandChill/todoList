517675c8a4d1574546a26bd78a4282723f35c11f
JustCodeandChill <inforreceiver@gmail.com>
2022-06-07 23:39:59 -0400

    6/7/22: Add basic scaffold for aside list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { SIDE_LIST_HEADER, LIST_DATA } from "./constants/index";
 
 const App = (): JSX.Element => {
+  const [list, setList] = useState(LIST_DATA);
   return (
     <>
-      <div>Hello world!</div>
+      <article>
+        <header>{SIDE_LIST_HEADER}</header>
+        <section>
+          <ul>
+            {list.map((listItem, index) => {
+              return <li key={index}>{listItem}</li>;
+            })}
+          </ul>
+        </section>
+      </article>
     </>
   );
 };
---
+++ src/constants/index.tsx
@@ -0,0 +1,2 @@
+export const SIDE_LIST_HEADER = "My Lists";
+export const LIST_DATA: string[] = ["Clean the house", "Go to the Gym", "Cook dinner"];
