94694f5112c92e99f32b36ab32ee4c2f3c34e8fd
JustCodeandChill <inforreceiver@gmail.com>
2022-06-17 00:40:54 -0400

    dev: add type for constants and list element

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import { SIDE_LIST_HEADER, LIST_DATA } from "./constants/index";
 
+interface Todo {
+  id: number;
+  todo: string;
+}
+
 const App = (): JSX.Element => {
-  const [list, setList] = useState(LIST_DATA);
+  const [list, setList] = useState<Todo[]>(LIST_DATA);
   return (
     <>
       <article>
         <header>{SIDE_LIST_HEADER}</header>
         <section>
           <ul>
-            {list.map((listItem, index) => {
-              return <li key={index}>{listItem}</li>;
+            {list.map(({ id, todo }): React.ReactElement => {
+              return <li key={id}>{todo}</li>;
             })}
           </ul>
         </section>
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,2 +1,15 @@
 export const SIDE_LIST_HEADER = "My Lists";
-export const LIST_DATA: string[] = ["Clean the house", "Go to the Gym", "Cook dinner"];
+export const LIST_DATA: { id: number; todo: string }[] = [
+  {
+    id: 1,
+    todo: "Clean the house"
+  },
+  {
+    id: 2,
+    todo: "Clean the house"
+  },
+  {
+    id: 3,
+    todo: "Clean the house"
+  }
+];
